5336d8552635bb77f50f5ab32f0e77559d6429c3
Jesse <jessegoodrum@me.com>
2023-02-07 16:17:48 -0800

    responsiveness to homepage

--- src/routes/home/home.components.jsx
+++ src/routes/home/home.components.jsx
@@ -15,9 +15,13 @@ export default function Home(){
                 <img src={jesse} alt="pic of jesse" className='image'/>
                 
                 <div className='text-container'>
-                    <h1>Hello! <br /> My name is Jesse Goodrum</h1>
-                    <h1>I'm a Full Stack Developer based in Seattle, WA</h1>
-                    <p> I recently discovered a passion for web development and have been actively pursuing it since early 2022. My skills include Java, JavaScript, SQL, React, Redux, Bootstrap, HTML5, CSS, Spring Boot, PostgreSQL, and JUnit. I have a creative mindset and an interest in technology and recently graduated from an accelerated full stack web development program through Merit America. I am continuously learning and improving my skills in my field..</p>
+                    <div className='header-container'>
+                    <h1 className='about-header'>Hello!</h1>
+                    <h1 className='about-header'>My name is Jesse Goodrum</h1>
+                    <h1 className='about-header'>I'm a Full Stack Developer</h1>
+                    <h1 className='about-header'>based in Seattle, WA</h1>
+                    </div>
+                    <p className='about-paragraph'> I recently discovered a passion for web development and have been actively pursuing it since early 2022. My skills include Java, JavaScript, SQL, React, Redux, Bootstrap, HTML5, CSS, Spring Boot, PostgreSQL, and JUnit. I have a creative mindset and an interest in technology and recently graduated from an accelerated full stack web development program through Merit America. I am continuously learning and improving my skills in my field..</p>
                 </div>
 
             </div>
